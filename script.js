03cecac3b9c2e892b05a88ec7bdc1cae674afa50
Marcelo854 <marcelolicco1@hotmail.com>
2024-12-14 15:39:59 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function openModal(type) {
+    if (type === 'consulta') {
+        alert("Agende sua consulta preenchendo o formulário!");
+    } else if (type === 'planos') {
+        alert("Conheça nossos planos de saúde!");
+    }
+}
